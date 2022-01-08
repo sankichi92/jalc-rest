f5234fda287238b0fcd16f71441ff7f9855ac0e3
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2022-01-08 22:35:25 +0900

    bundle gem jalc-rest -t

---
+++ lib/jalc/rest/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Jalc
+  module Rest
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/jalc/rest_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Jalc::Rest do
+  it "has a version number" do
+    expect(Jalc::Rest::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
