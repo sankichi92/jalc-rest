67aadb071495ecb895791fd625066c2a6fe18341
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2022-01-08 23:59:12 +0900

    Improve JaLC::REST::Client#prefixes

--- lib/jalc/rest/client.rb
+++ lib/jalc/rest/client.rb
@@ -1,12 +1,44 @@
 # frozen_string_literal: true
 
 require 'faraday'
+begin
+  require 'faraday_middleware'
+rescue LoadError
+  # do nothing
+end
+
+require_relative 'version'
 
 module JaLC
   module REST
     class Client
-      def prefixes
-        Faraday.get('https://api.japanlinkcenter.org/prefixes')
+      BASE_URL = 'https://api.japanlinkcenter.org'
+
+      def initialize(base_url: BASE_URL, user_agent: "jalc-rest v#{VERSION}")
+        @base_url = base_url
+        @user_agent = user_agent
+      end
+
+      def prefixes(ra: nil, sort: nil, order: nil)
+        conn.get(
+          '/prefixes',
+          {
+            ra: ra,
+            sort: sort,
+            order: order,
+          }.compact,
+        )
+      end
+
+      private
+
+      def conn
+        @conn ||= Faraday.new(
+          url: @base_url,
+          headers: { 'User-Agent' => @user_agent },
+        ) do |f|
+          f.response :json
+        end
       end
     end
   end
--- spec/jalc/rest/client_spec.rb
+++ spec/jalc/rest/client_spec.rb
@@ -2,16 +2,31 @@
 
 RSpec.describe JaLC::REST::Client do
   describe '#prefixes' do
-    before do
-      stub_request(:get, 'https://api.japanlinkcenter.org/prefixes')
+    let(:client) { described_class.new }
+
+    context 'without args' do
+      before do
+        stub_request(:get, 'https://api.japanlinkcenter.org/prefixes')
+      end
+
+      it 'requests "GET /prefixes"' do
+        client.prefixes
+
+        expect(WebMock).to have_requested(:get, 'https://api.japanlinkcenter.org/prefixes')
+      end
     end
 
-    it 'executes a request to get DOI prefixes' do
-      client = described_class.new
+    context 'with args' do
+      before do
+        stub_request(:get, 'https://api.japanlinkcenter.org/prefixes').with(query: hash_including)
+      end
 
-      client.prefixes
+      it 'requests "GET /prefixes" with params' do
+        client.prefixes(ra: 'JaLC', sort: 'siteId', order: 'desc')
 
-      expect(WebMock).to have_requested(:get, 'https://api.japanlinkcenter.org/prefixes')
+        expect(WebMock).to have_requested(:get, 'https://api.japanlinkcenter.org/prefixes')
+                             .with(query: { ra: 'JaLC', sort: 'siteId', order: 'desc' })
+      end
     end
   end
 end
